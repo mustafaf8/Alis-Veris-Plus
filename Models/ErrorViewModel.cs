1807cf691da53ac8b4579ea7277f8856b73e4d62
mustafa nahsan <129125878+mustafaf8@users.noreply.github.com>
2024-10-09 18:31:45 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Alis_Veris_Plus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
